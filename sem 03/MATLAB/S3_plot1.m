0c51fce64a92b13749862420e997968621aaf7a5
subhendu17620 <subhendu17620@gmail.com>
2019-11-05 10:40:04 +0530

    update

--- sem 03/MATLAB/S3_plot1.m
+++
@@ -1,7 +0,0 @@
-x = linspace(0,4,100);
-f = @(x) x;
-y = f(x);
-I = [-8,8];
-y1 = repmat(f(x),1,diff(I)/4);
-x1 = linspace(-8,8,length(y1));
-plot(x1,y1)
--- sem 03/MP lab/assignment/calc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-start=`date +%s%N`
-for i in {1..1000}; do ./assign; done;
-end=`date +%s%N`
-echo "Execution Time : $(((end-start)/1000)) nanoseconds"
-
-
-
-
